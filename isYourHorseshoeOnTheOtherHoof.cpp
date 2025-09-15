717181b4df368209716c2ca69d894068625ca4c0
Andrea <a.a.a.orbegoso@gmail.com>
2025-09-14 20:54:24 -0500

    Update isYourHorseshoeOnTheOtherHoof.cpp

--- isYourHorseshoeOnTheOtherHoof.cpp
+++ isYourHorseshoeOnTheOtherHoof.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 typedef long long ll;
 int main(){
-    ll s1, s2, s3, s4; cin>>s1>>s2>>s3>>s4;
-    
+    ll s1, s2, s3, s4; cin>>s1>>s2>>s3>>s4; //have to get how many diff ones exist
+    ll count = 0;
+    if(s1==s2 || s1==s3 || s1==s4){count++;}
+    else if(s2==s3 || s2 == s4){count++;}
+    else if(s3==s4){count++;}
+    cout<<count;
 }
